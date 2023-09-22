6443e0ae6c3c62de0bd04e32db9f1781cf961fb8
TrishonBaidaya7399 <githubtrishon@gmail.com>
2023-09-22 21:10:48 +0600

    Footer part added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,7 +2,7 @@
 
 const Header = () => {
     return (
-<div className="navbar mx-[200px]">
+<div className="navbar">
   <div className="navbar-start">
     <div className="dropdown">
       <label tabIndex={0} className="btn btn-ghost lg:hidden">
