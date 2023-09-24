c45cdca86a6715d3ea11d4a55135fc9454465c01
TrishonBaidaya7399 <githubtrishon@gmail.com>
2023-09-24 12:38:32 +0600

    image links modified

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,10 +1,11 @@
 // import PropTypes from 'prop-types';
-
+import pageUnderConstruction from "../../assets/images/under construction.png"
 const Blog = () => {
     return (
-        <div>
-            <h1>Blogs</h1>
-        </div>
+    <div className="flex flex-col gap-6 items-center px-[200px]">
+        <h1 className="text-4xl font-extrabold pt-8 pb-2">Blogs page</h1>
+        <img src={pageUnderConstruction} alt="" className="w-[50%] rounded-xl mb-8"/>
+    </div>
     );
 };
 
--- src/Components/Statistics/Statistics.jsx
+++ src/Components/Statistics/Statistics.jsx
@@ -1,9 +1,10 @@
 // import PropTypes from 'prop-types';
-
+import pageUnderConstruction from "../../assets/images/under construction.png"
 const Statistics = () => {
     return (
-        <div>
-            <h1>Statistics page</h1>
+        <div className="flex flex-col gap-6 items-center px-[200px]">
+            <h1 className="text-4xl font-extrabold pt-8 pb-2">Statistics page</h1>
+            <img src={pageUnderConstruction} alt="" className="w-[50%] rounded-xl mb-8"/>
         </div>
     );
 };
